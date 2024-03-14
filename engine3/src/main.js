a2d6c0f439347fa4655cdd372030aceab8813aff
stepan <stepan.lidmila@gmail.com>
2024-03-14 16:05:07 +0100

    Test

--- engine3/src/main.js
+++ engine3/src/main.js
@@ -1,15 +1,30 @@
 
 function setup() {
-    size(800, 600);
+    size(1500, 1000);
+    background(51);
 
-    new GameObject(width / 2, height - 5)
-    .addComponent(new RectRenderer(width, 10, () => {
-        noStroke(0);
-        fill(0, 255, 0);
-    }))
-    .addComponent(new BoxCollider(width, 10))
+    new GameObject(width / 2,height -10)
+    .addComponent(new RectRenderer(width, 20))
+
+    new GameObject(width / 2,height -45)
+    .addComponent(new RectRenderer(50, 50))
+
+
+    new GameObject(width - 1000 ,height -150)
+    .addComponent(new RectRenderer(100, 20))
+
+
+    new GameObject(width / 2 ,height -300)
+    .addComponent(new RectRenderer(100, 20))
+
+
+    new GameObject(width / 2 + 300 ,height -450)
+    .addComponent(new RectRenderer(100, 20))
+
+
 }
 
 function draw() {
     background(51);
+
 }
